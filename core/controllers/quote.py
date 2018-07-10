0d246b11f2494f43df0f2cb8032bce481d9a2ecf
fodisi <fodisi@users.noreply.github.com>
2018-07-10 15:33:17 -0400

    created api/quote/<symbol> endpoint

--- core/controllers/quote.py
+++ core/controllers/quote.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, jsonify
+import json
 
-from core.model.asset_quote import AssetQuote
+from ..model.quote import Quote
+from ..serializer.quote_serializer import QuoteSerializer
 
-quote_ctrl = Blueprint('quote', __name__, url_prefix='/quote')
+
+quote_ctrl = Blueprint('quote', __name__)
 
 html_filename = 'quote.html'
 
@@ -13,7 +16,7 @@ def __quote(symbol):
     quote = None
     error = None
     try:
-        quote = AssetQuote.from_market_data(symbol)
+        quote = Quote.from_market_data(symbol)
         quote = {'exchange': quote.exchange,
                  'symbol': quote.symbol,
                  'price': quote.last_price}
@@ -23,9 +26,22 @@ def __quote(symbol):
     return render_template(html_filename, quote=quote, error=error)
 
 
-@quote_ctrl.route('/', methods=['GET', 'POST'])
+@quote_ctrl.route('/quote', methods=['GET', 'POST'])
 def show_quote():
     if request.method == 'GET':
         return render_template(html_filename)
     else:
         return __quote(request.form['symbol'])
+
+
+@quote_ctrl.route('/api/quote/<symbol>', methods=['GET'])
+def api_quote(symbol):
+    try:
+        quote = Quote.from_market_data(symbol)
+        return QuoteSerializer().jsonify(quote)
+        # quote2 = {'exchange': quote.exchange,
+        #           'symbol': quote.symbol,
+        #           'price': quote.last_price}
+        # return json.dumps(quote.__dict__)
+    except Exception as e:
+        return jsonify(e.args[0])
